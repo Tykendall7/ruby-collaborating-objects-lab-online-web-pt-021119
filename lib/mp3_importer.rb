92241021c9a1aea60533a3825583b34ab36550e7
Tymeckia Kendall <tykend@gmail.com>
2019-03-24 22:40:35 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,9 @@
 class MP3Importer
   attr_accessor :path
   
+  
+  def initialize (path)
+    
     def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
