648e150fd8b7c3a8a7d03812bbde3b1a2a81a259
Tymeckia Kendall <tykend@gmail.com>
2019-03-24 22:41:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,14 @@
 class Song
     attr_accessor :name, :artist
+    
     def initialize (name)
-    @name = name
-  end
+      @name = name
+    end
     def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-  end
+      song = Song.new(name)
+      @songs << song
+      song.artist = self
+    end
   def self.new_by_filename(filename)
     song = self.new
     song.title = filename.split(" - ")[1]
