a932bced7ef0e20311c4392663c3f3eb75a2ecbd
Tymeckia Kendall <tykend@gmail.com>
2019-03-24 22:42:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,11 +4,7 @@ class Song
     def initialize (name)
       @name = name
     end
-    def add_song_by_name(name)
-      song = Song.new(name)
-      @songs << song
-      song.artist = self
-    end
+
   def self.new_by_filename(filename)
     song = self.new
     song.title = filename.split(" - ")[1]
