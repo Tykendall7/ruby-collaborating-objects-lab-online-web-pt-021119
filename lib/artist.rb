af15f7986ecb15b8d9e54fb3cf16104ef2398bd2
Tymeckia Kendall <tykend@gmail.com>
2019-03-24 22:37:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,22 @@
 class Artist
-  attr_accessor :name, :songs, :song, :artist
+  attr_accessor :name, :songs
   @@all = []
   
   def initialize (name)
     @name = name
+    @song = []
   end
+  
+  def self.all
+    @@all
+  end
+  
   def add_song(song)
     @songs << song
-    song.artist = self
   end
  
   def save
-   
+   @@all << self
   end
 
 end
